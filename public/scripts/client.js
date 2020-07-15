2b72742b4c1ad6d23f1ffdc3b6016cec39001c15
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-07-15 15:31:48 -0400

    fetching tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,35 +3,10 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// Test / driver code (temporary). Eventually will get this from the server.
 
-// Fake data taken from initial-tweets.json
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
- 
- const createTweetElement = function(tweetData) {
+const createTweetElement = function(tweetData) {
+  //clear the container before to read all tweets
+  $("#tweets-container").empty();
    // markup
  let $tweet = `<article>
  <header id="tweet-header">
@@ -77,10 +52,22 @@ $("#form").on("submit", function(event) {
     success: function (data) {
       //console.log to see if request was successful
       console.log("ajax request was successful");
-        renderTweets(data);
+        loadTweets(data);
       },
     error: function () {}
 
   });
 });
+//function that load tweets from db, and show on screen (call renderTweets function)
+const loadTweets = function() {
+
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    success: function (data) {
+      renderTweets(data);
+      },
+    error: function () {}
+  });
+};
 });
