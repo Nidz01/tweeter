54bc0dd18b84eefc4b5e783c5c471f1d6b5a50a9
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-07-15 22:40:48 -0400

    added option to clear container before rendering

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,88 +4,99 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = function(tweetData) {
+const createTweetElement = function (tweetData) {
   //clear the container before to read all tweets
   //$("#tweets-container").empty();
-   // markup
- let $tweet = `<article>
- <header id="tweet-header">
-   <img class="logo" src=${tweetData.user.avatars} />
-   <h2 class="name">${tweetData.user.name}</h2>
-   <span class="style-name">${tweetData.user.handle}</span>
- </header>
- <div>${tweetData.content.text}</div>
- <footer>X days ago</footer>
-</article>
+  // markup
+  let tweet = `
+  <article>
+  <header id="tweet-header">
+    <img class="logo" src=${tweetData.user.avatars} />
+    <h2 class="name">${tweetData.user.name}</h2>
+    <span class="style-name">${tweetData.user.handle}</span>
+  </header>
+  <div>${tweetData.content.text}</div>
+  <footer>X days ago</footer>
+  </article>
  `;
- return $tweet;
- }
+  return tweet;
+};
 
- const renderTweets = function(tweets) {
-// loops through tweets
-for (let i = 0; i < tweets.length; i++) {
+const renderTweets = function (tweets) {
+  
+  //clear the container before to read all tweets
+  $("#tweets-container").empty();
 
-  // calls createTweetElement for each tweet
-  $tweet = createTweetElement(tweets[i]);
+  // loops through tweets
+  for (let i = 0; i < tweets.length; i++) {
 
-  // takes return value and appends it to the tweets container
-  $('#tweets-container').append($tweet);
-}
- }
+    // calls createTweetElement for each tweet
+    let tweet = createTweetElement(tweets[i]);
 
-$(document).ready( function() {
+    // takes return value and appends it to the tweets container
+    $('#tweets-container').append(tweet);
+  }
+};
 
+$(document).ready(function () {
   //click event on textarea
-  $("#tweet-text").on("click", function(event) {
+  $("#tweet-text").on("click", function (event) {
     $('#error-message').text("");
     $(this).val('');
   });
+
   //event listener to submit button
-  $("#form").on("submit", function(event) {
-  //prevent to change the page
-  event.preventDefault();
+  $("#form").on("submit", function (event) {
 
-  //const textArea = $(this).find('#tweet-text');
+    //prevent to change the page
+    event.preventDefault();
 
-  //prepare data for Ajax calling
-  const data = $(this).serialize();
+    //const textArea = $(this).find('#tweet-text');
 
-  //data validation before sending it to server
-  //slice method to remove unwanted strings in start of input
-  const validationData = data.slice(5);
+    //prepare data for Ajax calling
+    const data = $(this).serialize();
 
-  if (validationData.length > 140) {
-    $('#error-message').text("Your message is too long!");
-  }
-  else if (validationData === "" || validationData === null) {
-    $('#error-message').text("Your message is empty!");
-  } else {
+    //data validation before sending it to server
+    //slice method to remove unwanted strings in start of input
+    const validationData = data.slice(5);
+
+    if (validationData.length > 140) {
+
+      $('#error-message').text("Your message is too long!");
+
+    } else if (validationData === "" || validationData === null) {
+
+      $('#error-message').text("Your message is empty!");
+    
+    } else {
+
       //ajax post request to send data to server
-  $.ajax({
-    url:"/tweets/",
+      $.ajax({
+        url: "/tweets/",
         method: "POST",
         data: data,
-    success: function (data) {
-        loadTweets(data);
-      },
-    error: function () {}
-  });
-  $('#error-message').text("");
-  }
-});
-//function that load tweets from db, and show on screen (call renderTweets function)
-const loadTweets = function() {
+        success: function (data) {
+          loadTweets(data);
+        },
+        error: function () { }
+      });
 
-  $.ajax({
-    url: '/tweets',
-    method: 'GET',
-    success: function (data) {
-      renderTweets(data);
-      },
-    error: function () {}
+      $('#error-message').text("");
+    }
   });
-};
-//start the app showing the tweets
-loadTweets();
+  //function that load tweets from db, and show on screen (call renderTweets function)
+  const loadTweets = function () {
+
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function (data) {
+        renderTweets(data);
+      },
+      error: function () { }
+    });
+  };
+  //start the app showing the tweets
+  loadTweets();
 
 });
