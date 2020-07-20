7143f80b598d08fa21f0a5360811ae67ba41ec31
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-07-20 10:52:12 -0400

    Refactored code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,23 +2,27 @@
 
 $(document).ready(function() {
   $(".new-tweet").on( "keyup keypress", "textarea" , function(event) {
-    //get the value of 'this', that mean, the text that user enter in a textarea
-    //get the length of the text.
-    //subtract this value from 140.
+
+    //get the value of 'this', that mean, the text that user entered in a textarea
     let text = $(this).val();
+
+
+    //get the length of the text and subtract this value from 140.
     let charsLeft = 140 - text.length;
 
     //go to parent element (form) and find the respective counter
-    //set the text to be equal the value calculated above
+
     let counter = $(this).closest("form").find(".counter");
 
-      //if the text exceed the 140 limit, that mean the variable charsLeft < 0,
-    //add a class to the counter that change the font color to red
-    //otherwise, remove that class to let the font color as original
+    //set the text to be equal the value calculated above
+    counter.text(charsLeft);
+
+    //if the text exceed the 140 limit, that mean the variable charsLeft < 0,
+    
     if (charsLeft < 0) {
-      counter.addClass("fontRed");
+      counter.addClass("fontRed"); //add a class to the counter that change the font color to red
     } else {
-      counter.removeClass("fontRed");
+      counter.removeClass("fontRed"); //otherwise, remove that class to let the font color as original
     }
   });
 });
